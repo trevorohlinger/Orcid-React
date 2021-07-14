56aefb75d5de5476d6471084a1a22df0c01a86a6
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-07-14 16:31:19 -0600

    Added check for success and a redirect to success page when successful.

--- src/components/Access.js
+++ src/components/Access.js
@@ -55,6 +55,7 @@ class Access extends Component {
   // window.location.href='https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=https://localhost:3000/access'
      window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
  //  window.location.href=`${API_URL}success`
+    success = true;
 
   /* const orcidCode = queryString.parse(window.location.search)
          sessionStorage.setItem("orcidCode", orcidCode) !!! */
@@ -77,7 +78,10 @@ class Access extends Component {
       }
     })
     .then (res => console.log("result returned ",res))
-    
+    console.log("success = ", (success));
+    if (success){
+      window.location.href=`${API_URL}success`
+    }
   //  window.location.href=`https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid`
 //    window.location.href=`${API_URL}success`
     //  window.location.href='https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid?code=' + values.code
@@ -164,6 +168,7 @@ class Access extends Component {
   render() {
     return (
         <h1>Access</h1>
+
     );
   }
 }
