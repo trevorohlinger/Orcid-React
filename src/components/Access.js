f98f6e102c7148a94a6ae21a28b5b187fab8d5e7
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-07-21 17:03:18 -0600

    Changed boolean to false before redirect now :)

--- src/components/Access.js
+++ src/components/Access.js
@@ -87,8 +87,8 @@ class Access extends Component {
     //  This is not currently working properly. This is the last thing I worked on as of 7-14-2021
     if (sessionStorage.getItem("success")){
     //  window.location.href=`${API_URL}success`
-      window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
       sessionStorage.setItem("success", false)
+      window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access` 
     }
 //    window.location.href=`https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid`
 //    window.location.href=`${API_URL}success`
