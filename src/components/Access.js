ca5277d34887407e81f72de976cb2aa6c1dd596c
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-08-13 16:52:28 -0600

    cleaned shit up

--- src/components/Access.js
+++ src/components/Access.js
@@ -4,7 +4,7 @@ import queryString from 'query-string'
 import axios from 'axios';
 import { isCompositeComponent } from 'react-dom/test-utils';
 
-const values = queryString.parse(window.location.search)
+
 
 
 class Access extends Component {
@@ -26,6 +26,8 @@ class Access extends Component {
 
     componentDidMount() {
 
+      const values = queryString.parse(window.location.search)
+
       const API_URL = process.env.REACT_APP_API_URL;
 
       console.log(JSON.stringify(values.access_token))
@@ -38,29 +40,9 @@ class Access extends Component {
         // employeeID is here
         console.log(parsedToken["urn:oid:1.2.840.113556.1.2.610"])
         sessionStorage.setItem("parsed_token", parsedToken["urn:oid:1.2.840.113556.1.2.610"])
+        this.setState({success: true})      
+        window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
 
-        this.setState({success: true})
-
-        //console.log(this.parseJwt(values.access_token))
-          /*1. Extract employeeNumber from access token. --Complete
-            2. Check DynamoDb using a query for employeeNumber. If it already exists skip we are done.
-            3. if not continue getting the Orcid access token.
-          */  
-        /*  do this from lambda
-        grant permissions inside aws to post to dynamodb
-            aws dynamodb put-item \
-            --table Orcid-Data-Boise-State-University \
-            --item parsedToken["urn:oid:1.2.840.113556.1.2.610"] \
-            --condition-expression "attribute_not_exists(Id)" */
-        
-    
-            // window.location.href='https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=https://localhost:3000/access'
-    
-            //   window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access2`
-            
-    //      window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access2`
-    // window.location.href=`${API_URL}success`
-  /*
       } else if (values.code)
         {
         console.log("We are inside of else if (values.code)")
@@ -87,7 +69,6 @@ class Access extends Component {
           window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}success` 
     //  }
     //    window.location.href=`${API_URL}success`
-  */
       } else {
           console.log("User denied authorization or authorization has failed. Attempting to get permission again.")
           window.location.href=`${API_URL}denied`
