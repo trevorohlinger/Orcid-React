817f02d8d1c24bb4f7913182134e3155f7f448be
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-08-25 15:45:49 -0600

    Slightly edited access.js to see if git is working properly

--- src/components/Access.js
+++ src/components/Access.js
@@ -60,6 +60,7 @@ class Access extends Component {
           console.log("result returned ",res)
           //window.location.href=`${API_URL}success`
           window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}success`
+          // Changing something just to see if it shows up in git.
         })
       //  This is not currently working properly. This is the last thing I worked on as of 7-14-2021
      // if (sessionStorage.getItem("success") && sessionStorage.getItem("access_token_complete")){
