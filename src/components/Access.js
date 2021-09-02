8bef5613236fbabf9e2b3d6b90f1ab2ddd5955fe
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-09-02 15:03:35 -0600

    Updated else if to check value of state.success

--- src/components/Access.js
+++ src/components/Access.js
@@ -31,26 +31,26 @@ class Access extends Component {
 
    if (values.code)
         {
-        console.log("We are inside of else if (values.code)")
-        //console.log("parsed_token =", sessionStorage.getItem("parsed_token"))
-        axios({
-          method: 'post',
-          url: 'https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid',
-          headers: {
-        //    'Authorization': `Basic ${values.access_token}`
-        //    'Authorization': `Bearer ${sessionStorage.getItem("access_token")}`
-        },
-        data: {
-          code          :  values.code,
-          redirect_uri  : `${API_URL}access`,
-          employeeID   : this.parseJwt(sessionStorage.getItem("access_token"))["urn:oid:1.2.840.113556.1.2.610"]
-        }
-        })
-        .then (res => {
-          console.log("result returned ",res)
-          //window.location.href=`${API_URL}updateDB`
-          window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}updateDB`
-        })
+          console.log("We are inside of else if (values.code)")
+          //console.log("parsed_token =", sessionStorage.getItem("parsed_token"))
+          axios({
+            method: 'post',
+            url: 'https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid',
+            headers: {
+          //    'Authorization': `Basic ${values.access_token}`
+          //    'Authorization': `Bearer ${sessionStorage.getItem("access_token")}`
+          },
+          data: {
+            code          :  values.code,
+            redirect_uri  : `${API_URL}access`,
+            employeeID   : this.parseJwt(sessionStorage.getItem("access_token"))["urn:oid:1.2.840.113556.1.2.610"]
+          }
+          })
+          .then (res => {
+            console.log("result returned ",res)
+            //window.location.href=`${API_URL}updateDB`
+            window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}updateDB`
+          })
 
     //  This is not currently working properly. This is the last thing I worked on as of 7-14-2021
     //  if (sessionStorage.getItem("success") && sessionStorage.getItem("access_token_complete")){
@@ -61,7 +61,7 @@ class Access extends Component {
     //  }
     //    window.location.href=`${API_URL}success`
       
-      } else if (values.access_token /* && !success */)
+      } else if (values.access_token && this.state.success == false )
         {
           sessionStorage.setItem("access_token", values.access_token)
           //let parsedToken = this.parseJwt(values.access_token)
