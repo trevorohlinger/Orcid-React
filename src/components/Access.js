972a886f3692bb82b110eb973afe40c5df427f3f
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-09-27 15:46:54 -0600

    Updated redirect back to \access

--- src/components/Access.js
+++ src/components/Access.js
@@ -51,7 +51,7 @@ class Access extends Component {
           --condition-expression "attribute_not_exists(Id)" */
       
   // window.location.href='https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=https://localhost:3000/access'
-     window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/updateDB`
+     window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
     
 
       /* const orcidCode = queryString.parse(window.location.search)
