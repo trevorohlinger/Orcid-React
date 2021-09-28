3891b810f63643d633b7b3f371c4fed4e1f3c874
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-09-28 10:16:49 -0600

    Attempting fix at employeeID pass to Lambda.

--- src/components/Access.js
+++ src/components/Access.js
@@ -85,92 +85,20 @@ class Access extends Component {
     .then (res => console.log("result returned ",res))
     console.log("success = ", (sessionStorage.getItem("success")));
     //This is not currently working properly. This is the last thing I worked on as of 7-14-2021
-    if (sessionStorage.getItem("success")){
-    //  window.location.href=`${API_URL}success`
-    
-    }
-  //  window.location.href=`https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid`
-//    window.location.href=`${API_URL}success`
-    //  window.location.href='https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid?code=' + values.code
-
- /*    const requestOptions = {
-      method: 'POST', 
-     // headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' },
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded', 
-        'Access-Control-Allow-Headers' : 'Content-Type',
-        'Access-Control-Allow-Origin'  : 'https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid?code=',
-        // https://gentle-inlet-06589.herokuapp.com/https://sandbox.orcid.org/oauth/token
-        'Access-Control-Allow-Methods' : 'OPTIONS,POST,GET', 
-     },
-      data: JSON.stringify({
-         client_id: 'APP-RASOJQY62Z86Q8CU',
-         client_secret : 'df79d593-8392-46ca-9f4a-2fb4cb109655',
-         grant_type : 'authorization_code',
-         code: values.code, 
-         redirect_uri  : 'https://localhost:3000'}) 
-    };  
-    fetch('https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid?code=', requestOptions)
-        .then(response => {
-          console.log('response= ',response)
-        }) */
- // Local POST Request. Not the way we want this done in the future, but works to get the initial authorization code for testing.
-  /*      const requestOptions = {
-        method: 'POST', 
-       // headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' },
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded', 
-          'Access-Control-Allow-Headers' : 'Content-Type',
-          'Access-Control-Allow-Origin'  : ' https://pub.sandbox.orcid.org/oauth/token',
-          // https://gentle-inlet-06589.herokuapp.com/https://sandbox.orcid.org/oauth/token
-          'Access-Control-Allow-Methods' : 'OPTIONS,POST,GET', 
-       },
-        data: JSON.stringify({
-           client_id: 'APP-RASOJQY62Z86Q8CU',
-           client_secret : 'df79d593-8392-46ca-9f4a-2fb4cb109655',
-           grant_type : 'authorization_code',
-           code: values.code, 
-           redirect_uri  : 'https://localhost:3000'}) 
-      };  
-      fetch('https://pub.sandbox.orcid.org/oauth/token', requestOptions)
-          .then(response => {
-            console.log('response= ',response)
-          }) */
-       /*   const response = await fetch('https://sandbox.orcid.org/oauth/token', requestOptions);
-           const data = await response.json(); 
-           this.setState({ postId: data.id });  */
-    } else {
+  } else if (values.access_token && this.state.success == false)
+        {
+          sessionStorage.setItem("access_token", values.access_token)
+          //let parsedToken = this.parseJwt(values.access_token)
+          //employeeID is here
+          //console.log(parsedToken["urn:oid:1.2.840.113556.1.2.610"])
+          //sessionStorage.setItem("parsed_token", parsedToken["urn:oid:1.2.840.113556.1.2.610"])
+          this.setState({success: true})      
+          window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
+      } else {
         console.log("User denied authorization or authorization has failed. Attempting to get permission again")
         window.location.href=`${API_URL}denied`
-    } 
-// 
-    //sessionStorage.setItem("access_token_parsed", parseJwt(values.access_token))
-
-    // ------------------------------------------- important -------------------------------- sessionStorage.setItem("id_token", values.id_token) 
-      
-    //  sessionStorage.setItem("access_token", values.access_token.employeeNumber)
-    //  window.sessionStorage.getItem("access_token")
-    //  console.log("access_token =",data.access_token)
-
-	  // then save query param to local storage
-	  //  sessionStorage.setItem("token", queryParams.token)
-	  //  console.log("queryParams =",queryParams);
-    }
-
- //   setData() {
-
-      /*
-      // https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid?code=
-        .post("https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/orcid", body)
-        .then(function(response) {
-          console.log(response.data);
-        })
-        .catch(function(error) {
-          console.log(error);
-      });  */
-  //  } 
+      } 
+  }
 
   render() {
     return (
