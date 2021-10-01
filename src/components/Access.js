f53e0da777a948df5617e5310708cb569f0078f7
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-10-01 12:20:40 -0600

    Added a success page redirect after the axios post

--- src/components/Access.js
+++ src/components/Access.js
@@ -45,8 +45,6 @@ class Access extends Component {
       this.state.success = true;
       sessionStorage.setItem("success", true)  
 
-
-      
   // window.location.href='https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=https://localhost:3000/access'
      window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
   //window.location.href=`https://spm35eaceb.execute-api.us-west-2.amazonaws.com/dev/updateDB`
@@ -74,6 +72,7 @@ class Access extends Component {
     .then (res => console.log("result returned ",res))
     console.log("success = ", (sessionStorage.getItem("success")));
     //This is not currently working properly. This is the last thing I worked on as of 7-14-2021
+    window.location.href=`${API_URL}success`
   } else if (values.access_token && this.state.success == false)
         {
           sessionStorage.setItem("access_token", values.access_token)
@@ -83,11 +82,11 @@ class Access extends Component {
           //sessionStorage.setItem("parsed_token", parsedToken["urn:oid:1.2.840.113556.1.2.610"])
           this.setState({success: true})      
           window.location.href=`https://sandbox.orcid.org/oauth/authorize?client_id=APP-RASOJQY62Z86Q8CU&response_type=code&scope=/read-limited%20/activities/update%20/person/update&redirect_uri=${API_URL}access`
-      } else {
-        console.log("User denied authorization or authorization has failed. Attempting to get permission again")
-        window.location.href=`${API_URL}denied`
-      } 
-  }
+  } else {
+    console.log("User denied authorization or authorization has failed. Attempting to get permission again")
+    window.location.href=`${API_URL}denied`
+  } 
+}
 
   render() {
     return (
