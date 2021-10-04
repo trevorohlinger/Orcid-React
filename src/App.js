ec4f6608222fb757a4056e98a6d34f59af58de5e
trevorohlinger <TrevorOhlinger@boisestate.edu>
2021-10-04 17:41:18 -0600

    Updated to ES6 backtick syntax for href in app.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
             <li>
             {/* <Link to="/logon">logon</Link> */}
             <strong><u>
-            <a href={'https://spm35eaceb.execute-api.us-west-2.amazonaws.com/' + ENV_FLAG + '/login'}>
+            <a href={`https://spm35eaceb.execute-api.us-west-2.amazonaws.com/${ENV_FLAG}/logon`}>
               Click here to create or connect your ORCID iD </a></u></strong>
             </li>
           </ul>
